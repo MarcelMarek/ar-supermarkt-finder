b772ef05dcce3a9521e8c59a378a58c5a3c9b4d3
MarcelMarek <114317273+MarcelMarek@users.noreply.github.com>
2024-05-28 22:01:22 +0200

    fixed material (getEngine error) not being able to be added to scene

--- src/typescript/products.ts
+++ src/typescript/products.ts
@@ -1,31 +1,39 @@
-import { AbstractMesh, MeshBuilder, ShadowGenerator, StandardMaterial, Texture, Vector4 } from "@babylonjs/core";
+import { AbstractMesh, MeshBuilder, Scene, StandardMaterial, Texture, Vector4 } from "@babylonjs/core";
 
-const height = 1;
-const width = 0.75;
-const depth = 0.25;
-
-const mat = new StandardMaterial("mat");
-const texture = new Texture("./assets/numbers.png");
-mat.diffuseTexture = texture;
-
-const columns = 6;
-const rows = 1;
-const faceUV = new Array(6);
-
-for (let i = 0; i < 6; i++) {
-  faceUV[i] = new Vector4(i / columns, 0, (i + 1) / columns, 1 / rows);
+function createMaterial(scene: Scene) {
+  const mat = new StandardMaterial("Material", scene);
+  const texture = new Texture("./assets/numbers.png", scene);
+  mat.diffuseTexture = texture;
+  return mat;
 }
 
-const exampleRandomOptions = {
-  faceUV: faceUV,
-  wrap: true,
-  height: height,
-  width: width,
-  depth: depth,
-};
+function createMesh(scene: Scene) {
+  const height = 1;
+  const width = 0.75;
+  const depth = 0.25;
 
-export function getExampleRandomMesh(shadowGenerator: ShadowGenerator) {
-  const exampleRandomMesh = MeshBuilder.CreateBox("box", exampleRandomOptions) as AbstractMesh;
-  shadowGenerator.addShadowCaster(exampleRandomMesh, true);
+  const mat = createMaterial(scene);
+
+  const columns = 6;
+  const rows = 1;
+  const faceUV = new Array(6);
+
+  for (let i = 0; i < 6; i++) {
+    faceUV[i] = new Vector4(i / columns, 0, (i + 1) / columns, 1 / rows);
+  }
+
+  const exampleRandomOptions = {
+    faceUV: faceUV,
+    wrap: true,
+    height: height,
+    width: width,
+    depth: depth,
+  };
+
+  return MeshBuilder.CreateBox("box", exampleRandomOptions, scene);
+}
+
+export function getExampleRandomMesh(scene: Scene) {
+  const exampleRandomMesh = createMesh(scene);
   return exampleRandomMesh;
 }
